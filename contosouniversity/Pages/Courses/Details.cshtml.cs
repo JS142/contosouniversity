8bc594d12ad2dd860270c52c64429cbb60993a35
Jobanjeet Singh <JS142@atcltd.atc.org.nz>
2021-02-26 14:20:06 +1300

    Add project files.

---
+++ contosouniversity/Pages/Courses/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+using contosouniversity.Data;
+
+namespace contosouniversity.Pages.Courses
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly contosouniversity.Data.contosouniversityContext _context;
+
+        public DetailsModel(contosouniversity.Data.contosouniversityContext context)
+        {
+            _context = context;
+        }
+
+        public Course Course { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Course = await _context.Course.FirstOrDefaultAsync(m => m.CourseID == id);
+
+            if (Course == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ contosouniversity/Pages/Enrollments/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+using contosouniversity.Data;
+
+namespace contosouniversity.Pages.Enrollments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly contosouniversity.Data.contosouniversityContext _context;
+
+        public IndexModel(contosouniversity.Data.contosouniversityContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Enrollment> Enrollment { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Enrollment = await _context.Enrollment
+                .Include(e => e.Course)
+                .Include(e => e.Student).ToListAsync();
+        }
+    }
+}
